703523a866b24a73a70fc31971a5258b29722ca2
Matheus <goca1377@gmail.com>
2021-03-11 21:50:53 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -51,4 +51,26 @@ describe('LogControllerDecorator', () => {
 
     expect(handleSpy).toHaveBeenLastCalledWith(httpRequest)
   })
+
+  it('should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Matheus'
+      }
+    })
+  })
 })
