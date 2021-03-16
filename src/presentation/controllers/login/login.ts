faa531560baf6de73bfabd2df9915b1bd6f54bc2
Matheus <goca1377@gmail.com>
2021-03-15 22:44:43 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, Validation, Authentication } from './login-protocols'
-import { ok, badRequest, serverError, unauthorized } from '../../helpers/http-helper'
+import { ok, badRequest, serverError, unauthorized } from '../../helpers/http/http-helper'
 
 export class LoginController implements Controller {
   private readonly validation: Validation
