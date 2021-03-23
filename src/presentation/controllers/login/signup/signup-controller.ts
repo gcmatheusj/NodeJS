757fba244840a9c3af3cfe3f9ac3dc858d737c29
Matheus <goca1377@gmail.com>
2021-03-22 21:05:41 -0300

    refactor: move login and signup controllers to a folder

--- src/presentation/controllers/login/signup/signup-controller.ts
+++ src/presentation/controllers/login/signup/signup-controller.ts
@@ -1,6 +1,6 @@
 import { HttpResponse, HttpRequest, Controller, AddAccount, Validation, Authentication } from './signup-controller-protocols'
-import { badRequest, serverError, ok, forbidden } from '../../helpers/http/http-helper'
-import { EmailInUseError } from '../../errors/email-in-user-error'
+import { badRequest, serverError, ok, forbidden } from '../../../helpers/http/http-helper'
+import { EmailInUseError } from '../../../errors/email-in-user-error'
 
 export class SignUpController implements Controller {
   constructor (
