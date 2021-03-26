4b2fc749a0f525fb3c860bdf78a2bf4aa6d43b40
Matheus <goca1377@gmail.com>
2021-03-25 22:08:37 -0300

    refactor: organize main layer folders

---
+++ src/main/factories/use-cases/account/authentication/db-authentication-factory.ts
@@ -0,0 +1,17 @@
+import env from '../../../../config/env'
+import { DbAuthentication } from '../../../../../data/use-cases/authentication/db-authentication'
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-repository'
+import { BcryptAdapter } from '../../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { Authentication } from '../../../../../domain/use-cases/authentication'
+
+export const makeDbAuthentication = (): Authentication => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+
+  const accountMongoRepository = new AccountMongoRepository()
+
+  return new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+}
---
+++ src/main/factories/use-cases/survey/add-survey/db-add-survey-factory.ts
@@ -0,0 +1,9 @@
+import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
+import { AddSurvey } from '../../../../../domain/use-cases/add-survey'
+import { DbAddSurvey } from '../../../../../data/use-cases/add-survey/db-add-survey'
+
+export const makeDbAddSurvey = (): AddSurvey => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+
+  return new DbAddSurvey(surveyMongoRepository)
+}
