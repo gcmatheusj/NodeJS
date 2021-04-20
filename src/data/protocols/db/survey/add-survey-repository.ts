3fc144fff81c839576e8fd06096fac5c6ba547df
Matheus <goca1377@gmail.com>
2021-04-20 20:40:44 -0300

    feat: add route to serve static files

--- src/data/protocols/db/survey/add-survey-repository.ts
+++ src/data/protocols/db/survey/add-survey-repository.ts
@@ -1,5 +1,5 @@
 import { AddSurveyParams } from '@/domain/use-cases/survey/add-survey'
 
 export interface AddSurveyRepository {
-  add: (surveyData: AddSurveyParams) => Promise<void>
+  add: (data: AddSurveyParams) => Promise<void>
 }
--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setupStaticFiles from './static-file'
 import setupRoutes from './routes'
 import setupSwagger from './swagger'
 
 const app = express()
 
+setupStaticFiles(app)
 setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
